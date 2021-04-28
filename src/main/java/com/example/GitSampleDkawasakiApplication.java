296b1e330db777d0f7bedc312863a21cdf70f80b
dkawasaki <daisuke.kawasaki@rakus-partners.co.jp>
2021-04-28 10:46:26 +0900

    新規にファイルを作成

---
+++ src/main/java/com/example/GitSampleDkawasakiApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleDkawasakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleDkawasakiApplication.class, args);
+		
+		
+		System.out.println("Githubのサンプルだよーーん");
+	}
+
+}
